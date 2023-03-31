5fe291833a1c84994348a6b86db40026aaf255f2
Johan Nordlander <johan.nordlander@dataductus.se>
2023-03-31 08:21:54 +0200

    Replaced toB_int by to$int.

--- src/nsoapi/utils.ext.c
+++ src/nsoapi/utils.ext.c
@@ -16,7 +16,7 @@ nsoapiQ_utilsQ_IPv4Address nsoapiQ_utilsQ_IPv4AddressD_try_parse (B_str s) {
     int r = inet_pton(AF_INET, fromB_str(s), &addr);
     if (r == 0) {
         return nsoapiQ_utilsQ_IPv4AddressG_new($NEWTUPLE(4,
-            toB_int(addr.s_addr >> 24 & 0xff), toB_int(addr.s_addr >> 16 & 0xff), toB_int(addr.s_addr >> 8 & 0xff), toB_int(addr.s_addr & 0xff)));
+            to$int(addr.s_addr >> 24 & 0xff), to$int(addr.s_addr >> 16 & 0xff), to$int(addr.s_addr >> 8 & 0xff), to$int(addr.s_addr & 0xff)));
     }
     return B_None;
 }
@@ -27,14 +27,14 @@ nsoapiQ_utilsQ_IPv6Address nsoapiQ_utilsQ_IPv6AddressD_try_parse (B_str s) {
     int r = inet_pton(AF_INET6, fromB_str(s), &addr);
     if (r == 0) {
         return nsoapiQ_utilsQ_IPv6AddressG_new($NEWTUPLE(8,
-            toB_int(ntohs(addr.s6_addr16[0])),
-            toB_int(ntohs(addr.s6_addr16[1])),
-            toB_int(ntohs(addr.s6_addr16[2])),
-            toB_int(ntohs(addr.s6_addr16[3])),
-            toB_int(ntohs(addr.s6_addr16[4])),
-            toB_int(ntohs(addr.s6_addr16[5])),
-            toB_int(ntohs(addr.s6_addr16[6])),
-            toB_int(ntohs(addr.s6_addr16[7]))));
+            to$int(ntohs(addr.s6_addr16[0])),
+            to$int(ntohs(addr.s6_addr16[1])),
+            to$int(ntohs(addr.s6_addr16[2])),
+            to$int(ntohs(addr.s6_addr16[3])),
+            to$int(ntohs(addr.s6_addr16[4])),
+            to$int(ntohs(addr.s6_addr16[5])),
+            to$int(ntohs(addr.s6_addr16[6])),
+            to$int(ntohs(addr.s6_addr16[7]))));
     }
     return B_None;
 }
