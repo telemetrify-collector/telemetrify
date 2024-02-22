c9003346613320440b043d0318f3d34f77785415
Ciaran Cain <ccain@dev.terastrm.net>
2024-02-22 22:09:31 +0000

    Syntax and integration fixes

--- src/tsdb/prometheus.ext.c
+++ src/tsdb/prometheus.ext.c
@@ -1,41 +1,46 @@
 
 #include "prw.pb-c.h"
 
-B_bytes prometheusQ_pack(B_list metric_data) {
+void tsdbQ_prometheusQ___ext_init__() {
+   // NOP
+}
+
+B_bytes tsdbQ_prometheusQ_pack(B_list metric_data) {
     WriteRequest write_request;
     write_request__init(&write_request);
 
-    TimeSeries **time_series = calloc(metric_data.length, sizeof(TimeSeries*));
-    for (int i = 0; i < metric_data.length; ++i) {
-        B_tuple metric_series = (B_tuple)metric_data.data[i];
+    TimeSeries **time_series = calloc(metric_data->length, sizeof(TimeSeries*));
+    for (int i = 0; i < metric_data->length; ++i) {
+        B_tuple metric_series = (B_tuple)metric_data->data[i];
         TimeSeries *new_series = malloc(sizeof(TimeSeries));
 	time_series__init(new_series);
 
-	B_list labels = (B_list)metric_series.components[0]
-        new_series->n_labels = labels.length;
-	Label **labels_pb = calloc(labels.length, sizeof(Label*));
-	for (int j = 0; j < labels.length; ++j) {
+	B_list labels = (B_list)metric_series->components[0];
+        new_series->n_labels = labels->length;
+	Label **labels_pb = calloc(labels->length, sizeof(Label*));
+	for (int j = 0; j < labels->length; ++j) {
             Label* new_label = malloc(sizeof(Label));
 	    label__init(new_label);
-            B_tuple label = (B_tuple)labels.data[j];
+            B_tuple label = (B_tuple)labels->data[j];
 
-	    new_label->name = ((B_str)label.components[0]).str;
-	    new_label->value = ((B_str)label.components[1]).str;
+	    // Hopefully no encoding weirdness but in theory bytes are bytes
+	    new_label->name = (char*)((B_str)label->components[0])->str;
+	    new_label->value = (char*)((B_str)label->components[1])->str;
 
 	    labels_pb[j] = new_label;
 	}
 	new_series->labels = labels_pb;
 
-	B_list samples = (B_list)metric_series.components[1]
-        new_series->n_samples = samples.length;
-	Sample **samples_pb = calloc(samples.length, sizeof(Samples*));
-	for (int j = 0; j < samples.length; ++j) {
+	B_list samples = (B_list)metric_series->components[1];
+        new_series->n_samples = samples->length;
+	Sample **samples_pb = calloc(samples->length, sizeof(Sample*));
+	for (int j = 0; j < samples->length; ++j) {
             Sample* new_sample = malloc(sizeof(Sample));
 	    sample__init(new_sample);
-            B_tuple sample = (B_tuple)samples.data[j];
+            B_tuple sample = (B_tuple)samples->data[j];
 
-	    new_sample->value = ((B_float)sample.components[0]).val;
-	    new_sample->timestamp = from$int((B_int)sample.components[1]);
+	    new_sample->value = ((B_float)sample->components[0])->val;
+	    new_sample->timestamp = from$int((B_int)sample->components[1]);
 
 	    samples_pb[j] = new_sample;
 	}
@@ -46,9 +51,9 @@ B_bytes prometheusQ_pack(B_list metric_data) {
 
     write_request.timeseries = time_series;
 
-    size_t buffer_size = write_request__get_packed_size(write_request);
+    size_t buffer_size = write_request__get_packed_size(&write_request);
     uint8_t *buffer = malloc(buffer_size);
-    write_request__pack(write_request, buffer);
+    write_request__pack(&write_request, buffer);
 
     B_bytes ret = to$bytesD_len((char*)buffer, buffer_size);
 
