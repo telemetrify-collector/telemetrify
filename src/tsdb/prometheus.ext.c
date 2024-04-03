31cfb82fb6d3cd473f5dc31f1deade83645ccbf9
Kristian Larsson <kristian@spritelink.net>
2024-04-03 09:03:56 +0200

    Use acton_malloc & acton_calloc

--- src/tsdb/prometheus.ext.c
+++ src/tsdb/prometheus.ext.c
@@ -10,18 +10,18 @@ B_bytes tsdbQ_prometheusQ_pack(B_list metric_data) {
     WriteRequest write_request;
     write_request__init(&write_request);
 
-    TimeSeries **time_series = calloc(metric_data->length, sizeof(TimeSeries*));
+    TimeSeries **time_series = acton_calloc(metric_data->length, sizeof(TimeSeries*));
     write_request.n_timeseries = metric_data->length;
     for (int i = 0; i < metric_data->length; ++i) {
         B_tuple metric_series = (B_tuple)metric_data->data[i];
-        TimeSeries *new_series = malloc(sizeof(TimeSeries));
+        TimeSeries *new_series = acton_malloc(sizeof(TimeSeries));
         time_series__init(new_series);
 
         B_list labels = (B_list)metric_series->components[0];
         new_series->n_labels = labels->length;
-        Label **labels_pb = calloc(labels->length, sizeof(Label*));
+        Label **labels_pb = acton_calloc(labels->length, sizeof(Label*));
         for (int j = 0; j < labels->length; ++j) {
-            Label* new_label = malloc(sizeof(Label));
+            Label* new_label = acton_malloc(sizeof(Label));
             label__init(new_label);
             B_tuple label = (B_tuple)labels->data[j];
 
@@ -35,9 +35,9 @@ B_bytes tsdbQ_prometheusQ_pack(B_list metric_data) {
 
         B_list samples = (B_list)metric_series->components[1];
         new_series->n_samples = samples->length;
-        Sample **samples_pb = calloc(samples->length, sizeof(Sample*));
+        Sample **samples_pb = acton_calloc(samples->length, sizeof(Sample*));
         for (int j = 0; j < samples->length; ++j) {
-            Sample* new_sample = malloc(sizeof(Sample));
+            Sample* new_sample = acton_malloc(sizeof(Sample));
             sample__init(new_sample);
             B_tuple sample = (B_tuple)samples->data[j];
 
@@ -58,7 +58,7 @@ B_bytes tsdbQ_prometheusQ_pack(B_list metric_data) {
 
     size_t buffer_size = write_request__get_packed_size(&write_request);
     //printf("buffer_size: %zu\n", buffer_size);
-    uint8_t *buffer = malloc(buffer_size);
+    uint8_t *buffer = acton_malloc(buffer_size);
     write_request__pack(&write_request, buffer);
 
     B_bytes ret = to$bytesD_len((char*)buffer, buffer_size);
