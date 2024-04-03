c380dedbd497eb7b29f86fc463b818d2c20079f2
Kristian Larsson <kristian@spritelink.net>
2024-04-03 09:03:09 +0200

    Fix indent

--- src/tsdb/prometheus.ext.c
+++ src/tsdb/prometheus.ext.c
@@ -15,40 +15,40 @@ B_bytes tsdbQ_prometheusQ_pack(B_list metric_data) {
     for (int i = 0; i < metric_data->length; ++i) {
         B_tuple metric_series = (B_tuple)metric_data->data[i];
         TimeSeries *new_series = malloc(sizeof(TimeSeries));
-	time_series__init(new_series);
+        time_series__init(new_series);
 
-	B_list labels = (B_list)metric_series->components[0];
+        B_list labels = (B_list)metric_series->components[0];
         new_series->n_labels = labels->length;
-	Label **labels_pb = calloc(labels->length, sizeof(Label*));
-	for (int j = 0; j < labels->length; ++j) {
+        Label **labels_pb = calloc(labels->length, sizeof(Label*));
+        for (int j = 0; j < labels->length; ++j) {
             Label* new_label = malloc(sizeof(Label));
-	    label__init(new_label);
+            label__init(new_label);
             B_tuple label = (B_tuple)labels->data[j];
 
-	    // Hopefully no encoding weirdness but in theory bytes are bytes
-	    new_label->name = (char*)((B_str)label->components[0])->str;
-	    new_label->value = (char*)((B_str)label->components[1])->str;
+            // Hopefully no encoding weirdness but in theory bytes are bytes
+            new_label->name = (char*)((B_str)label->components[0])->str;
+            new_label->value = (char*)((B_str)label->components[1])->str;
 
-	    labels_pb[j] = new_label;
-	}
-	new_series->labels = labels_pb;
+            labels_pb[j] = new_label;
+        }
+        new_series->labels = labels_pb;
 
-	B_list samples = (B_list)metric_series->components[1];
+        B_list samples = (B_list)metric_series->components[1];
         new_series->n_samples = samples->length;
-	Sample **samples_pb = calloc(samples->length, sizeof(Sample*));
-	for (int j = 0; j < samples->length; ++j) {
+        Sample **samples_pb = calloc(samples->length, sizeof(Sample*));
+        for (int j = 0; j < samples->length; ++j) {
             Sample* new_sample = malloc(sizeof(Sample));
-	    sample__init(new_sample);
+            sample__init(new_sample);
             B_tuple sample = (B_tuple)samples->data[j];
 
-	    new_sample->value = ((B_float)sample->components[0])->val;
-	    new_sample->timestamp = from$int((B_int)sample->components[1]);
+            new_sample->value = ((B_float)sample->components[0])->val;
+            new_sample->timestamp = from$int((B_int)sample->components[1]);
 
-	    samples_pb[j] = new_sample;
-	}
-	new_series->samples = samples_pb;
+            samples_pb[j] = new_sample;
+        }
+        new_series->samples = samples_pb;
 
-	time_series[i] = new_series;
+        time_series[i] = new_series;
     }
 
     write_request.timeseries = time_series;
